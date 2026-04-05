9aea35deb3cc64a6a0c97ab1b4fb8add2b86bc34
wleka <116182950+wleka@users.noreply.github.com>
2026-04-05 11:02:37 +0500

    Transfering all source to github repository

---
+++ src/main/kotlin/org/example/wleku/kappaEvents/FartCommand.kt
@@ -0,0 +1,23 @@
+package org.example.wleku.kappaEvents
+
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+class FartCommand: CommandExecutor {
+    override fun onCommand(
+        sender: CommandSender,
+        p1: Command,
+        p2: String,
+        p3: Array<out String>
+    ): Boolean {
+
+        if (sender is Player) {
+            sender.sendMessage("Вы пернули только что.")
+            sender.world.playSound(sender.location, org.bukkit.Sound.ENTITY_SHULKER_SHOOT, 1.5f, 1f)
+        }
+
+        return true
+    }
+}
---
+++ src/main/kotlin/org/example/wleku/kappaEvents/KappaEvents.kt
@@ -0,0 +1,21 @@
+package org.example.wleku.kappaEvents
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class KappaEvents : JavaPlugin() {
+
+    companion object{
+        lateinit var instance: KappaEvents
+            private set
+    }
+
+    override fun onEnable() {
+        // Plugin startup logic
+
+        getCommand("fart")?.setExecutor(FartCommand())
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}

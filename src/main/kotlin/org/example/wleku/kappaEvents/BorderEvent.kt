434bc97fa56bd9f5b16eb089a18fac9bcea4d528
wleka <116182950+wleka@users.noreply.github.com>
2026-05-04 19:08:15 +0500

    Updated and fixed bugs

--- src/main/kotlin/org/example/wleku/kappaEvents/BorderEvent.kt
+++ src/main/kotlin/org/example/wleku/kappaEvents/BorderEvent.kt
@@ -1,10 +1,16 @@
 package org.example.wleku.kappaEvents
 
+import org.bukkit.Location
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerMoveEvent
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
 
 class BorderEvent: Listener {
+
+    val maxBlock = 5000
+    val warningActionBar = maxBlock - 25
 
     @EventHandler
     fun onBorder(event: PlayerMoveEvent) {
@@ -12,5 +18,55 @@ class BorderEvent: Listener {
         val xBlock = player.location.blockX
         val yBlock = player.location.blockY
         val zBlock = player.location.blockZ
+        val world = event.player.world
+
+        if (world.name == "world") {
+            if (xBlock >= warningActionBar || xBlock <= -warningActionBar || zBlock <= -warningActionBar || zBlock >= warningActionBar) {
+                player.sendActionBar("Вы находитесь рядом с границей мира.")
+
+                if (xBlock >= maxBlock) {
+                    player.sendActionBar("Вы переходите границу мира.")
+                    player.teleport(
+                        Location(
+                            player.world,
+                            xBlock.toDouble() - 1.0,
+                            yBlock.toDouble(),
+                            zBlock.toDouble()
+                        )
+                    )
+                } else if (zBlock >= maxBlock) {
+                    player.sendActionBar("Вы переходите границу мира.")
+                    player.teleport(
+                        Location(
+                            player.world,
+                            xBlock.toDouble(),
+                            yBlock.toDouble(),
+                            zBlock.toDouble() - 1.0
+                        )
+                    )
+                } else if (xBlock <= -maxBlock) {
+                    player.sendActionBar("Вы переходите границу мира.")
+                    player.teleport(
+                        Location(
+                            player.world,
+                            xBlock.toDouble() + 1.0,
+                            yBlock.toDouble(),
+                            zBlock.toDouble()
+                        )
+                    )
+                } else if (zBlock <= -maxBlock) {
+                    player.sendActionBar("Вы переходите границу мира.")
+                    player.teleport(
+                        Location(
+                            player.world,
+                            xBlock.toDouble(),
+                            yBlock.toDouble(),
+                            zBlock.toDouble() + 1.0
+                        )
+                    )
+                }
+            }
+        }
+
     }
 }
--- src/main/kotlin/org/example/wleku/kappaEvents/TeleportBackroomsEvent.kt
+++ src/main/kotlin/org/example/wleku/kappaEvents/TeleportBackroomsEvent.kt
@@ -1,8 +1,82 @@
 package org.example.wleku.kappaEvents
 
+import org.bukkit.Bukkit
+import org.bukkit.Location
+import org.bukkit.Material
+import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockBreakEvent
+import org.bukkit.event.block.BlockPlaceEvent
+import org.bukkit.event.player.PlayerMoveEvent
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
 
 class TeleportBackroomsEvent: Listener {
 
+    val nameWorld: String = "world_backrooms"
 
+    val blocksList = arrayListOf<Material>(
+        Material.SEA_LANTERN,
+        Material.DARK_OAK_PLANKS,
+        Material.STRIPPED_BIRCH_LOG,
+        Material.POLISHED_ANDESITE,
+        Material.SMOOTH_SANDSTONE,
+        Material.DARK_OAK_STAIRS
+    )
+
+    @EventHandler
+    fun onTeleportBackroomsEvent(event: PlayerMoveEvent) {
+        val player = event.player
+        val blockX = player.location.x
+        val blockY = player.location.y
+        val blockZ = player.location.z
+        val world = player.world
+
+        if (world.name == "world") {
+            if (blockY <= -72) {
+                Bukkit.broadcastMessage("${player.name} выпал из мира.")
+                player.teleport(Location(Bukkit.getWorld(nameWorld), 0.0, -59.0, 0.0))
+                player.health = 20.0
+                player.addPotionEffect(PotionEffect(PotionEffectType.DARKNESS, Int.MAX_VALUE, 0))
+            }
+        } else if (world.name == nameWorld) {
+            if (blockY >= -27) {
+                Bukkit.broadcastMessage("${player.name} вёрнулся в мир.")
+                player.teleport(Location(Bukkit.getWorld("world"), 0.0, 71.0, 0.0))
+                player.health = 20.0
+                player.removePotionEffect(PotionEffectType.DARKNESS)
+            }
+        }
+
+    }
+
+    @EventHandler
+    fun onDisableBreak(event: BlockBreakEvent) {
+        val player = event.player
+        val world = player.world.name
+        val block = event.block
+
+        if (world == nameWorld) {
+            for (blockDisaled in blocksList) {
+                if (block.type == blockDisaled) {
+                    event.isCancelled = true
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    fun onDisblePlace(event: BlockPlaceEvent) {
+        val player = event.player
+        val world = player.world.name
+        val block = event.block
+
+        if (world == nameWorld) {
+            for (blockDisaled in blocksList) {
+                if (block.type == blockDisaled) {
+                    event.isCancelled = true
+                }
+            }
+        }
+    }
 }
